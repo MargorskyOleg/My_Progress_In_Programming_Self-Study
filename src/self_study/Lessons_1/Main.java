73c37ab877bc8092da4198f6589d1ea5e53c4f42
MargorskyOleg <margorskyoleg@gmail.com>
2018-09-26 03:58:26 +0300

    Edit index "e"

--- src/self_study/Lessons_1/Main.java
+++ src/self_study/Lessons_1/Main.java
@@ -1,12 +1,11 @@
 package self_study.Lessons_1;
 
-
 public class Main {
     // First, solve the problem. Then, write the code.
     // считает позиции всех букв "e"
+    // e 22 30 44 56 72 80 90
     //FzirrBsDtP,m UsJoOlQvXef xtUhiep LpYr`oUbVlhe_mg.O WTshMeKnK,J nwhr^iotVeM ztRhQek McVo`dRer.W
     public static void main(String[] args) {
-
 
         String s = "FzirrBsDtP,m UsJoOlQvXef xtUhiep LpYr`oUbVlhe_mg.O WTshMeKnK,J nwhr^iotVeM ztRhQek McVo`dRer.W";
         int lenn = s.length();                  //94   "Fzirr
@@ -21,20 +20,16 @@ public class Main {
 
 
         int index1 = s.indexOf("e");            //
+        char[] let = new char[index1];
         while (index1 < s.length()){            //
+
             if(index1 == s.indexOf("e")){
 
-                System.out.print(s.indexOf("e")+" ");
+                System.out.print(index1+" ");
             }
             index1++;                           //
         }
-        System.out.println();                   //souf следуешия строка
-
-
-
-
-
-
+        System.out.println(index1);                   //souf следуешия строка
 
         int a = 0;
         while(a < 10) {
