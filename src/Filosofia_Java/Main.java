233365f0ec2d60253088ecfb41807ae625e21689
margorskyoleg@gmail.com <margorskyoleg@gmail.com>
2018-10-03 22:36:34 +0300

    Практика из Filosofia Java Книги

---
+++ src/Filosofia_Java/Main.java
@@ -0,0 +1,159 @@
+package Filosofia_Java;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
+//стр.70
+//class AllTheColorsOfTheRainbow{
+//    int anIntegerRepresentngColors;
+//    void changeTheHueOfTheColor(int newHue){
+//        //...
+//    }
+//    //..
+//}
+
+//стр.69
+//:object/HelloDate.java
+//import java.util.*;
+///**Первая программа-пример книги.
+// * Выводит строку и текущее число.
+// * @author Брюс Элккель
+// * @author www.MindView.net
+// * @version 4.0
+// */
+//public class Main{
+//    /** Точка входа в класс и приложение
+//     * @param args Массив строковых фргументов
+//     * @throws ecxeptions Исключения не выдаются
+//     */
+//    public static void main(String[] args) {
+//        System.out.println("Привет, сегодня: ");
+//        System.out.println(new Date());
+//
+//    }
+//}
+
+//стр.66
+////:object/Documentation2.java
+///**
+// * Можно <em>даже</em> встаыить список:
+// * <ol>
+// *     <li>Пункт первый</>
+// *     <li>Пункт второй</>
+// *     <li>Пункт третий</>
+// * </ol>
+// */
+/////:~
+
+//стр.66
+////:object/Documentation2.java
+///**
+// *<>
+// *     System.out.ptintln(new Date())
+// *</>
+// */
+
+//стр.66
+////.object/Documentationl.java
+///**Комментарий к классу*/
+//public class Documentationl{
+//    /**Комментарий к переменной*/
+//    public int i;
+//    /**Комментарий к методу*/
+//    public void f(){}
+//}///;~
+
+//стр.62
+//HelloDate.java
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Привет, сегодня;");
+//        System.out.println(new Date());
+
+//стр.61
+//Incremenrble.increment();
+
+//стр.61
+//Incrementable sf = new Incrementable();
+//sf.increment();
+
+//стр.61
+//class Incremenrable{
+//    static void increment(){StaticTest i++;}
+//}
+
+//стр.61
+//        StaticTest i++;
+
+//стр.61
+//        StaticTest st1 = new StaticTest();
+//        StaticTest st2 = new StaticTest();
+
+//стр.60
+//class StaticTest{
+//    static int i = 47;
+//}
+
+//стр.58
+//boolean flag(){return true;}
+//float naturelLogBase(){return 2.718}
+//void nothing(){return;}
+//void nothung2(){}
+
+//стр.58
+//        int storage(String s){
+//            return s.length()*2;
+//        }
+
+//стр.55
+//        class DataOnly{
+//            int i;
+//            double d;
+//            boolean b;
+//        }
+
+//стр.55
+//        ATypeName a = new ATypeName();
+
+//стр.55
+//        class ATypeName{/*Тело класса*/}
+
+//стр.54
+//        {
+//            String s = new String("строка");
+//        }        //конец области действия
+
+//стр.54
+//        {
+//            int x = 12;
+//            {
+//                int x = 96;//неверно
+//            }
+//        }
+
+//стр.53
+//        {
+//            int x = 12;
+//            //доступно только x
+//            {
+//                int q = 96;
+//                //доступны как x, так и q
+//            }
+//            //доступно только x
+//            //q надится "за пределами видимости"
+//        }
+
+//стр.52
+//Character ch = 'x';
+//char c = ch;
+
+//стр.51
+//        char c = 'x';
+//        Character ch = new Character(c);
+//    }
+//}
