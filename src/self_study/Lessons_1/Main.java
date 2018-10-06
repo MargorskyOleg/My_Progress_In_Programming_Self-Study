542dc196e5bbfbc93962e24aa50629d38f7ebd1c
margorskyoleg@gmail.com <margorskyoleg@gmail.com>
2018-10-06 23:56:16 +0300

    edit index "e"

--- src/self_study/Lessons_1/Main.java
+++ src/self_study/Lessons_1/Main.java
@@ -11,45 +11,50 @@ public class Main {
         int num = s.length();                           //число элиментов в num 94    спомошью length() можно подсчитать сколько символов встроке!
         char[] letter = new char[num];                  //[a][.][.]... = 1 2 3...
 
-        //int letter2 = s.indexOf("e");                   //"",letter.length = 94   "e",letter.length = -1    "e" = 22
-
-
         for (int i = 0; i < num; i=i+2) {                //+2 94->
             letter[i] = s.charAt(i);                    //s.charAt(i); символ в указанной позиции;
-//            letter2 = s.charAt(i);                      //letter2 = s.charAt(i); = 44
-//            System.out.print(letter2 +" ");           //:70 105 114 115 116 44 32 115 111 108 118 101 32 116 104 101 32 112 114 111 98 108 101 109 46 32 84 104 101 110 44 32 119 114 105 116 101 32 116 104 101 32 99 111 100 101 46
-//            System.out.print(i+" ");                  //:0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92
-            //            if(letter2 == s.indexOf("e",letter2)){
-            //                System.out.print(letter2+" ");          //:44 44
-            //            }
         }
-//        System.out.println(" ");                      //:" "
-//        System.out.print(letter2+" ");                //:46
 //        System.out.println(" ");                      //:" "
 //        System.out.print(letter);                     //:First, solve the problem. Then, write the code.
 //        System.out.println(" ");                      //:" "
 
-
         String result = new String(letter);
-
-
 //        System.out.print(result+" ");                 //:First, solve the problem. Then, write the code.
-        int num2 = 0;
-//        System.out.print(num2+" ");                    //:1
-        while (num2 < result.indexOf("e",num2+1)) {                                                       //result.indexOf("e",num2)) { //: 22 22 22 22 22 22 22               result.length()){
-        //    num2 = result.indexOf("e",num2+1);
-            System.out.print(num2 +" ");
 
-//            if(num2 == result.indexOf("e",num2)){
-//                System.out.print(num2+" ");              //:22 30 44 56 72 80 90
-//            }
+        int num2 = 0;
+        while (num2 < result.lastIndexOf("e",num2)){                                                                //()условия                   {}телоцыкла код который не обходимо выполнить до тех пор пока условия истено
+        //    num2 = result.indexOf("e",);
+            System.out.print(num2 +" ");                            //:22 30 44 56 72 80 90
         }
 
+//        int num2 = 0;
+//        while (num2 < result.lastIndexOf("e")){                                                                       //()условия                   {}телоцыкла код который не обходимо выполнить до тех пор пока условия истено
+//            num2 = result.indexOf("e",num2+1);
+//            System.out.print(num2 +" ");                          //:22 30 44 56 72 80 90
+//        }
+
+//        int num2 = 0;
+//        while (num2 < result.length()){                                                                               //()условия                   {}телоцыкла код который не обходимо выполнить до тех пор пока условия истено
+//            num2 = result.indexOf("e",num2+1);
+//            System.out.print(num2 +" ");                          //:22 30 44 56 72 80 90 -1 22 30 44 56 72 80 90 -1 22 30 44 56 72 80 90 -1
+//        }
+        
+//        int num2 = 0;
+//        int num3 = result.length();
+//        while (num2 < num3){                                                                                          //()условия                   {}телоцыкла код который не обходимо выполнить до тех пор пока условия истено
+//            num2 = result.indexOf("e",num2+1);
+//            System.out.print(num2 +" ");                          //:22 30 44 56 72 80 90 -1 22 30 44 56 72 80 90 -1 22 30 44 56 72 80 90 -1
+//        }
 
 
 
-
-
+//        int num2 = 0;
+//  //      System.out.print(num2+" ");
+//        while (num2 < result.indexOf("e",num2+1)) {                                                                   //()условия                   {}телоцыкла
+//               num2 = result.indexOf("e",num2+1);
+////        //    System.out.print(result.indexOf("e",num2+1) +" ");
+////            System.out.print(num2 +" ");
+//        }
 
 
 //        String result = new String(letter);
@@ -65,8 +70,6 @@ public class Main {
 //        }
 
 
-
-
 //        int index = s.indexOf("e");
 //        while (index < s.length()){
 //            if(index == s.indexOf("e",index)){
@@ -74,9 +77,6 @@ public class Main {
 //            }
 //            index++;
 //        }
-
-
-
 
 
 //        int num = s.length();
